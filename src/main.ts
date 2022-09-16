9a40053d7742d769366c603b39b6766eebb72a7c
Orca <orca.zhang@yahoo.com>
2022-09-17 06:00:20 +0800

    :key: feat: add notice

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { Button, Cell, CellGroup, Field } from 'vant';
+import { Button, Cell, CellGroup, Field, NoticeBar } from 'vant';
 
 const app = createApp(App);
 app.use(Button);
 app.use(Cell);
 app.use(CellGroup);
 app.use(Field);
+app.use(NoticeBar);
 app.mount('#app')
